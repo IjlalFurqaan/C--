7fb1aa0b3cfb6e45a88eee3f4a44fe8fd2a9e890
Ijlal Furqaan <45873251+IjlalFurqaan@users.noreply.github.com>
2024-09-06 11:18:42 +0200

    cpp

--- demo.cpp
+++ demo.cpp
@@ -1,8 +1,39 @@
-#include<iostream>
+#include <iostream>
+#include <vector>
 using namespace std;
 
-int main(){
-    int x;
-    cin>>x;
-    cout<<" Hey"<<x;
+class Node {
+public:
+    int data;  // the data value
+    Node* next;  // pointer to the next node
+
+    // Constructor with data and next node
+    Node(int data1, Node* next1 = nullptr) {
+        data = data1;
+        next = next1;
+    }
+};
+
+int main() {
+    int n;
+    cin >> n;  // Read the number of elements
+
+    vector<int> arr(n);
+
+    // Read array elements from input
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    // Creating a new Node with the first element of the array
+    Node* y = new Node(arr[0]);
+
+    // Outputting the pointer address and value
+    cout << y << '\n';         // Outputs the memory address of y
+    cout << y->data << '\n';   // Outputs the value stored in y (which is arr[0])
+
+    // Clean up dynamically allocated memory
+    delete y;
+
+    return 0;
 }
