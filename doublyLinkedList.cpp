9f1b6e52dca6ee7c29803c4f464afe189123e60a
Ijlal Furqaan <45873251+IjlalFurqaan@users.noreply.github.com>
2024-09-10 10:40:45 +0200

    deleteKthNode

--- doublyLinkedList.cpp
+++ doublyLinkedList.cpp
@@ -60,12 +60,71 @@ Node* deleteHead(Node* head)
     delete prev;
     return head;
 }
+Node* deleteTail(Node *head)
+{
+    if(head== NULL || head->next==NULL)
+    {
+        return NULL;
+    }
+    Node* tail=head;
+    while(tail->next!=NULL)
+    {
+        tail=tail->next;
+    }
+    Node* newTail=tail->back;
+    newTail->next=nullptr;
+    tail->back=nullptr;
+    
+    delete tail;
+    return head;
+}
+
+Node* deleteKthNode(Node* head, int k)
+{
+    if(head==NULL)
+    {
+        return NULL;
+    }
+    int cnt=0;
+    Node* kthNode=head;
+    while(kthNode!=NULL)
+    {
+        cnt++;
+        if(cnt==k) break;
+        kthNode=kthNode->next;
+    }
+    Node* prev=kthNode->back;
+    Node* front=kthNode->next;
+
+    if(prev==NULL && front==NULL)
+    {
+        return NULL;
+    }
+    else if(prev==NULL)
+    {
+        return deleteHead(head);
+    }
+    else if(front ==NULL)
+    {
+        return deleteTail(head);
+    }
+    prev->next=front;
+    front->back=prev;
+
+    kthNode->next=nullptr;
+    kthNode->back=nullptr;
+
+    delete kthNode;
+    return head;
+}
 
 int main(){
-    vector<int> arr= {1,0,1,2,3,4,5};
+    vector<int> arr= {9,7,6,5,8};
     Node* head =convertArraytoDll(arr);
 
-    head=deleteHead(head);
+    // head=deleteHead(head);
+    // head=deleteTail(head);
+     deleteKthNode(head,3);
     print(head);
 
 }
